33e2ef052cc90297cbeb295187645e5d99ddf55f
Trương Công Hoàng Long <truongconghoanglong@gmail.com>
2023-06-19 18:09:44 +0700

    refactor: change commonjs to modulejs

--- configs/env.config.js
+++ configs/env.config.js
@@ -1,11 +1,16 @@
-require('dotenv').config();
+const CLIENT_URL = process.env.DEV_CLIENT_URL || 'http://localhost:3000';
+const SERVER_URL = process.env.DEV_SERVER_URL || 'http://localhost:8080';
+const DB_CONNECTION_STRING =
+	process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017';
+const PORT = process.env.PORT || 8080;
+const CLIENT_ID = process.env.CLIENT_ID || '';
+const CLIENT_SECRET = process.env.CLIENT_SECRET || '';
 
-module.exports = {
-	CLIENT_URL: process.env.DEV_CLIENT_URL || 'http://localhost:3000',
-	SERVER_URL: process.env.DEV_SERVER_URL || 'http://localhost:8080',
-	DB_CONNECTION_STRING:
-		process.env.DB_CONNECTION_STRING || 'mongodb://locahost:27017',
-	PORT: process.env.PORT || 8080,
-	CLIENT_ID: process.env.CLIENT_ID || '',
-	CLIENT_SECRET: process.env.CLIENT_SECRET || '',
+export {
+	CLIENT_ID,
+	CLIENT_SECRET,
+	CLIENT_URL,
+	DB_CONNECTION_STRING,
+	PORT,
+	SERVER_URL,
 };
